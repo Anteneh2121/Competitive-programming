b18d4b19d1130bc708e2518bc95279f72927017c
Anteneh Bizuneh <88293613+Anteneh2121@users.noreply.github.com>
2022-10-19 05:59:38 -0700

    reduce-array-size-to-the-half

---
+++ reduce-array-size-to-the-half.py
@@ -0,0 +1,16 @@
+#reduce-array-size-to-the-half
+class Solution:
+    def minSetSize(self, arr: List[int]) -> int:
+        d = {}
+        for i in arr:
+            d[i] = d.get(i, 0) + 1
+        freq = sorted(d.values(), reverse=True)
+        cur = 0
+        n = 0
+        for f in freq:
+            cur += f
+            n += 1
+            if cur >= len(arr) // 2:
+                return n
+        
+        
