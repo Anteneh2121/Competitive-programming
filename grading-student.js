b9a400ab3f4184f15d1bd2d060e70a5d5ea78ba4
Anteneh Bizuneh <88293613+Anteneh2121@users.noreply.github.com>
2022-10-22 00:57:26 -0700

    grading students

--- grading-student.js
+++ grading-student.js
@@ -1,3 +1,27 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
 /*
  * Complete the 'gradingStudents' function below.
  *
@@ -6,7 +30,7 @@
  */
 
 function gradingStudents(grades) {
-    // Write your code here   
+    // Write your code here
     for (let i = 0; i < grades.length; i++) {        
         if (((grades[i] + 2) % 5 == 0) && (grades[i] + 2 > 39)) {
             grades[i] = grades[i] + 2;
@@ -15,4 +39,23 @@ function gradingStudents(grades) {
         }
     }
     return grades;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const gradesCount = parseInt(readLine().trim(), 10);
+
+    let grades = [];
+
+    for (let i = 0; i < gradesCount; i++) {
+        const gradesItem = parseInt(readLine().trim(), 10);
+        grades.push(gradesItem);
+    }
+
+    const result = gradingStudents(grades);
+
+    ws.write(result.join('\n') + '\n');
+
+    ws.end();
 }
