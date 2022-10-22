ed1a572e81a59a0ceb64b06ddc7ab42b1f388284
Anteneh Bizuneh <88293613+Anteneh2121@users.noreply.github.com>
2022-10-22 11:19:22 -0700

    insertion sort

--- Insertion-Sort-Part1.py
+++ Insertion-Sort-Part1.py
@@ -1,3 +1,19 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'insertionSort1' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY arr
+#
+
 def insertionSort1(n, arr):
     # Write your code here
     j = n-1
@@ -12,3 +28,10 @@ def insertionSort1(n, arr):
             print(' '.join(str(x) for x in arr))
             break 
     
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    insertionSort1(n, arr)
