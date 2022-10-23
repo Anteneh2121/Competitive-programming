de579204c96c2f6b7757403b375599e3df349e19
Anteneh Bizuneh <88293613+Anteneh2121@users.noreply.github.com>
2022-10-23 06:29:18 -0700

    numbers-are-smaller-than-the-current-number

--- Numbers-Smaller-Than-Current-Number.py
+++ Numbers-Smaller-Than-Current-Number.py
@@ -1,7 +1,8 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        indices = {}
-        for idx, num in enumerate(sorted(nums)):
-            indices.setdefault(num, idx)
-        return [indices[num] for num in nums]
-        
+        count = [0] * 102
+        for num in nums:
+            count[num+1] += 1
+        for i in range(1, 102):
+            count[i] += count[i-1]
+        return [count[num] for num in nums]
