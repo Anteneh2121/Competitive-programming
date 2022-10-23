8115178643fc2e49de43b41c184629d86ee66c11
Anteneh Bizuneh <88293613+Anteneh2121@users.noreply.github.com>
2022-10-23 03:07:40 -0700

    Selection-sort

--- Selection-Sort.py
+++ Selection-Sort.py
@@ -16,19 +16,3 @@ class Solution:
             temp = arr[i]
             arr[i] = arr[min_index]
             arr[min_index] = temp
-
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-if __name__ == '__main__': 
-    t = int(input())
-    for _ in range(t):
-        n = int(input())
-        arr = list(map(int, input().strip().split()))
-        Solution().selectionSort(arr, n)
-        for i in range(n):
-            print(arr[i],end=" ")
-        print()
-# } Driver Code Ends
