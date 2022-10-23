6da26fe65cbc6e8769846a9a2ef5462a0a653505
Anteneh Bizuneh <88293613+Anteneh2121@users.noreply.github.com>
2022-10-23 03:04:05 -0700

    selection-sort

--- Selection-Sort.py
+++ Selection-Sort.py
@@ -1,9 +1,12 @@
+#User function Template for python3
+#!/bin/python3
+
 """
 Selection Sort: https://practice.geeksforgeeks.org/problems/selection-sort/1
 """
 class Solution: 
     def selectionSort(self, arr,n):
-        for i in range(n):
+        for i in range(n-1):
             min_index = i
             
             for j in range(i+1, n):
@@ -13,3 +16,19 @@ class Solution:
             temp = arr[i]
             arr[i] = arr[min_index]
             arr[min_index] = temp
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        Solution().selectionSort(arr, n)
+        for i in range(n):
+            print(arr[i],end=" ")
+        print()
+# } Driver Code Ends
